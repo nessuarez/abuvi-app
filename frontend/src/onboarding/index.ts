ce085622e1147b915e30e45ea812bad92c7aab6e
Nestor Suarez Alfonso <nestor.suarez.alfonso@gmail.com>
2026-03-11 00:56:09 +0100

    refactor(onboarding): Disabling welcome tour

--- frontend/src/onboarding/index.ts
+++ frontend/src/onboarding/index.ts
@@ -1,10 +1,10 @@
 import { registerTour } from '@/composables/useOnboarding'
-import { welcomeTour } from './tours/welcome.tour'
+// import { welcomeTour } from './tours/welcome.tour'  // temporarily disabled
 import { registrationTour } from './tours/registration.tour'
 import { membershipTour } from './tours/membership.tour'
 import { campManagementTour } from './tours/camp-management.tour'
 
-registerTour(welcomeTour)
+// registerTour(welcomeTour)  // temporarily disabled
 registerTour(registrationTour)
 registerTour(membershipTour)
 registerTour(campManagementTour)
